8b6f0885e524f92816b7991cc8cf83504a66773e
Lee Jaehyeong <76482379+goldpink@users.noreply.github.com>
2022-09-21 15:45:02 +0900

    silver3, solved, 문제 더 명확히 파악

--- 1951.py
+++ 1951.py
@@ -1,20 +1,8 @@
-n = input()
+N = input()
 
-# 9 * (1 + 20 + 300 + 4000 + ...)
-val_len = [i+1 for i in range(len(n))]
-val_len.sort(reverse=True)
+answer=0
+for i in range(1,len(N)):
+    answer += 9*i*10**(i-1)
+answer += len(N)*(int(N) - 10**(len(N)-1) + 1)
 
-__sum=0
-for val in n:
-    _count=0
-    _sum=0
-    
-    # 
-    if val_len[0]!=1:
-        for i in val_len:
-            i -= 1
-            _count += 9*i*(10**(i-1))
-            _sum+=_count
-    __sum += int(val)*_sum
-    
-print(__sum)
+print(answer%1234567)
