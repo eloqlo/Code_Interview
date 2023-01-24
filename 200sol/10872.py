5660f65b911d75b5e423776fe3b124158f82841b
Lee Jaehyeong <76482379+goldpink@users.noreply.github.com>
2023-01-25 01:07:15 +0900

    all days

---
+++ 200sol/10872.py
@@ -0,0 +1,15 @@
+n = int(input())
+val = 1
+
+def printer(n):
+    global val
+    if n==0:
+        print(1)
+        return
+    
+    val*=n
+    if n==1:
+        return
+    printer(n-1)
+printer(n)    
+print(val)
