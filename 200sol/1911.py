f777d7a30456b7cd013309b930479b9b4b253fba
rrr-jh <dlwoguddd@gmail.com>
2023-02-01 02:11:29 +0900

    processing...

--- 200sol/1911.py
+++ 200sol/1911.py
@@ -1,3 +1,4 @@
+# 존나어렵, 흙길 채우기
 n, l = map(int, input().split())
 arr1=[]
 for _ in range(n):
@@ -7,25 +8,8 @@ for _ in range(n):
 arr1.sort()
 maximum = arr1[-1][1]
 count = 0
-left = 0
-# O(K)
-for idx,(s,e) in enumerate(arr1):
-    # calc count
-    num = e-s
-    if idx!=0:
-        # 다음순서로 넘어가는 애들 처리부분
-        tmptmp = s-last_index<left 
-        if tmptmp!=0:
-            num-=tmptmp
 
-    tmp = num//l
-    if num%l!=0:
-        tmp+=1
-    count += tmp
+# 너무 복잡하게 짠다. 다시 생각해보기.
 
-    # calc tmp
-    left = l-(num%l)
-    last_index = e
-    tmptmp=0
 
 print(count)
