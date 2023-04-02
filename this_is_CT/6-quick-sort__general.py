befa8980dc34bd689b694e8c2918753a6b1ae8d5
Lee Jaehyeong <76482379+goldpink@users.noreply.github.com>
2023-04-02 23:22:15 +0900

    프로그래머스 day4

--- this_is_CT/6-quick-sort__general.py
+++ this_is_CT/6-quick-sort__general.py
@@ -1,11 +1,11 @@
 arr = [9,2,4,7,6,3,6,5,0,1,8]
 
 def quick_sort(arr, start, end):
-    if start>=end:    # 원소가 1개면 종료
-        return          ## return 만으로 종료 가능하구나
-    pivot = start   # 첫 원소를 피봇
+    if start>=end:      # 원소가 1개면 종료
+        return          # return 만으로 종료 가능하구나
+    pivot = start       # 첫 원소를 피봇
     left = start + 1    # 첫 원소 옆이 left
-    right = end     # 마지막이 right
+    right = end         # 마지막이 right
     
     # 피봇 기준으로, 좌 우 정렬!
     while left <= right : 
