e95266c3efc0d7d155c10ab34c3b3a417f237501
rrr-jh <dlwoguddd@gmail.com>
2024-01-30 23:22:31 +0900

    pg lv0

--- 200sol/CodeInterview_book/8-2.py
+++ 200sol/CodeInterview_book/8-2.py
@@ -14,16 +14,25 @@ def computations(x):
 
 x = int(input())
 count=0
-dpTable={x}
-toCompute={x}
+dpTable={x}  # 이미 계산한 녀석들
+toCompute={x}  # 이번 count에서 계산할 녀석들
+flag=False
 for _ in range(300000):
-    count+=1
-    for ele in toCompute:
-        results=computations(ele)
+    count += 1
+    for ele in list(toCompute):
+        results = computations(ele)
         if 1 in results:
+            flag = True
             break
-        toCompute=results - dpTable
-        dpTable=dpTable.union(results)
-        
+        toCompute = toCompute|results   # 계산 예정 stack 추가
+        dpTable = dpTable|results   # 이번 계산 결과 DPtable에 추가
+    toCompute-=dpTable  # 안본 애들만 계산 다시 해준다.
+    if flag:
+        break
 
 print(count)
+
+
+"""
+책에선, N별로 1에 도달하는 가장 짧은 hops를 구하는 방식으로 O(N)로 풀음.
+"""
