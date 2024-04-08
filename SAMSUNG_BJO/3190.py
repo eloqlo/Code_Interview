52922912710ea2e71a6deda9d634a9bfd02e315e
rrr-jh <dlwoguddd@gmail.com>
2024-04-08 20:41:52 +0900

    minor change

--- SAMSUNG_BJO/3190.py
+++ SAMSUNG_BJO/3190.py
@@ -36,10 +36,10 @@ snake=[(1,1)]
 
 
 ################# FUNCTIONS ################
-def print_board(board, time):
+def print_board(board, t):
     global snake
     print()
-    print(f"______________ time : {time} _______________")
+    print(f"______________ time : {t} _______________")
     for line in board:
         for ele in line:
             print(f"{ele} ", end='')
@@ -48,120 +48,119 @@ def print_board(board, time):
 
     print("snake - ",snake)
 
-def rotate_board_L(board):
-    global snake, N
-    final_board=[]
-    dig_board=[]
-    fliped_board=[]
-    final_snake=[]
+# def rotate_board_L(board):
+#     global snake, N
+#     final_board=[]
+#     dig_board=[]
+#     fliped_board=[]
+#     final_snake=[]
+#
+#     # modify board: 반전 -> 대각화 -> 반전
+#     for line in board:
+#         fliped_board.append([line[idx] for idx in range(len(line)-1,-1,-1)])
+#     for line in zip(*fliped_board):
+#         dig_board.append(list(line))
+#     for line in dig_board:
+#         final_board.append([line[idx] for idx in range(len(line) - 1, -1, -1)])
+#
+#     # SANITY CHECK !
+#     # final_board2=[]
+#     # fliped_board2=[]
+#     # dig_board2=[]
+#     # for line in final_board:
+#     #     fliped_board2.append([line[idx] for idx in range(len(line)-1,-1,-1)])
+#     # for line in zip(*fliped_board2):
+#     #     dig_board2.append(list(line))
+#     # for line in dig_board2:
+#     #     final_board2.append([line[idx] for idx in range(len(line) - 1, -1, -1)])
+#     #
+#     # print("############################################")
+#     # print_board(board, 0)
+#     # print()
+#     # print_board(final_board2, 1)
+#     # print("############################################")
+#
+#     # failed..?
+#     # new_board= [["X"]*(N+2) for _ in range(N+2)]
+#     # for old_y in range(len(board)):
+#     #     for old_x in range(len(board)):
+#     #         new_y = N+1 - old_x
+#     #         new_x = N+1 - old_y
+#     #         new_board[new_y][new_x] = board[old_y][old_x]
+#
+#     # modify snake coordinate
+#     for y,x in snake:
+#         y_new = N+1 - x
+#         x_new = N+1 - y
+#         final_snake.append((y_new,x_new))
+#     snake = final_snake
+#     return final_board
+#
+# def rotate_board_D(board):
+#     global snake, N
+#     final_board = []
+#     fliped_board = []
+#     final_snake = []
+#
+#     # modify board: 반전 -> 대각화
+#     for line in board:
+#         fliped_board.append([line[idx] for idx in range(len(line) - 1, -1, -1)])
+#     for dig_line in zip(*fliped_board):
+#         final_board.append(list(dig_line))
+#
+#     # modify snake coordinate
+#     for y, x in snake:
+#         y_new = N + 1 - x   #TODO sanity check
+#         x_new = y
+#         final_snake.append((y_new, x_new))
+#         # sanity check
+#         if final_board[y_new][x_new] != "S":
+#             print(final_board[y_new][x_new])
+#             print('D flip, Snake position Erorr')
+#             print(board)
+#             print('snake: ', snake)
+#             print('old coordL ', y, x)
+#             print('new coord: ', y_new, x_new)
+#             raise
+#
+#     snake = final_snake
+#     return final_board
 
-    # modify board: 반전 -> 대각화 -> 반전
-    for line in board:
-        fliped_board.append([line[idx] for idx in range(len(line)-1,-1,-1)])
-    for line in zip(*fliped_board):
-        dig_board.append(list(line))
-    for line in dig_board:
-        final_board.append([line[idx] for idx in range(len(line) - 1, -1, -1)])
-
-    # SANITY CHECK !
-    # final_board2=[]
-    # fliped_board2=[]
-    # dig_board2=[]
-    # for line in final_board:
-    #     fliped_board2.append([line[idx] for idx in range(len(line)-1,-1,-1)])
-    # for line in zip(*fliped_board2):
-    #     dig_board2.append(list(line))
-    # for line in dig_board2:
-    #     final_board2.append([line[idx] for idx in range(len(line) - 1, -1, -1)])
-    #
-    # print("############################################")
-    # print_board(board, 0)
-    # print()
-    # print_board(final_board2, 1)
-    # print("############################################")
-
-    # failed..?
-    # new_board= [["X"]*(N+2) for _ in range(N+2)]
-    # for old_y in range(len(board)):
-    #     for old_x in range(len(board)):
-    #         new_y = N+1 - old_x
-    #         new_x = N+1 - old_y
-    #         new_board[new_y][new_x] = board[old_y][old_x]
-
-    # modify snake coordinate
-    for y,x in snake:
-        y_new = N+1 - x
-        x_new = N+1 - y
-        final_snake.append((y_new,x_new))
-    snake = final_snake
-    return final_board
-
-def rotate_board_D(board):
-    global snake, N
-    final_board = []
-    fliped_board = []
-    final_snake = []
-
-    # modify board: 반전 -> 대각화
-    for line in board:
-        fliped_board.append([line[idx] for idx in range(len(line) - 1, -1, -1)])
-    for dig_line in zip(*fliped_board):
-        final_board.append(list(dig_line))
-
-    # modify snake coordinate
-    for y, x in snake:
-        y_new = N + 1 - x   #TODO sanity check
-        x_new = y
-        final_snake.append((y_new, x_new))
-        # sanity check
-        if final_board[y_new][x_new] != "S":
-            print(final_board[y_new][x_new])
-            print('D flip, Snake position Erorr')
-            print(board)
-            print('snake: ', snake)
-            print('old coordL ', y, x)
-            print('new coord: ', y_new, x_new)
-            raise
-
-    snake = final_snake
-    return final_board
-
-def get_next_board(board, time):
+def get_next_board(board, t, d_idx):
     global commands, snake
+    # ['right','down','left','up']
+    dy=[0,1,0,-1]
+    dx=[1,0,-1,0]
+
     next_board = []
+    for idx in range(N + 2):
+        next_board.append(board[idx].copy())
+
     is_end = False
 
     # check if it's time to rotate
-    if time - 1 in commands["time"]:
-        idx = commands["time"].index(time - 1)
-        req_command = commands["command"][idx]
-    else:
-        req_command = None
-
-    # set "board", "snake" according to given command
-    if req_command == "L":
-        next_board = rotate_board_L(board)
-    elif req_command == "D":
-        next_board = rotate_board_D(board)
-    else:
-        # get "next_board" using "board"
-        for idx in range(N + 2):
-            next_board.append(board[idx].copy())
-
     # move snake to right.
     snake_head = snake[0]
-    nxt_y = snake_head[0]
-    nxt_x = snake_head[1]+1
+    if t - 1 in commands["time"]:
+        idx = commands["time"].index(t - 1)
+        req_command = commands["command"][idx]
+        if req_command == "D":
+            d_idx = (d_idx + 1) % 4
+        elif req_command == "L":
+            d_idx = (d_idx + 3) % 4
+
+    nxt_x = snake_head[1] + dx[d_idx]
+    nxt_y = snake_head[0] + dy[d_idx]
     if next_board[nxt_y][nxt_x] == "#":
         is_end=True
-        return next_board, is_end
+        return next_board, is_end, d_idx
     elif next_board[nxt_y][nxt_x] == "A":
         next_board[nxt_y][nxt_x] = "S"
         snake.insert(0,(nxt_y, nxt_x))
-        return next_board, is_end
+        return next_board, is_end, d_idx
     elif next_board[nxt_y][nxt_x] == "S":
         is_end = True
-        return next_board, is_end
+        return next_board, is_end, d_idx
     elif next_board[nxt_y][nxt_x] == ".":
         snake.insert(0,(nxt_y,nxt_x))
         next_board[nxt_y][nxt_x]= "S"
@@ -169,7 +168,7 @@ def get_next_board(board, time):
         tail_x=snake[-1][1]
         next_board[tail_y][tail_x]= "."
         snake.pop()
-        return next_board, is_end
+        return next_board, is_end, d_idx
     else:
         "ERROR"
         raise
@@ -177,15 +176,15 @@ def get_next_board(board, time):
 
 def solution(board):
 
-    for time in range(1,int(1e+9)):
-
-        board, is_end = get_next_board(board, time)
-        print_board(board,time)  #TODO debug..
+    d_idx=0
+    for t in range(1,int(1e+9)):
+        board, is_end, d_idx = get_next_board(board, t, d_idx)
+        # print_board(board,t)  #TODO debug..
 
         if is_end:
             break
 
-    return time
+    return t
 
 
 if __name__=='__main__':
