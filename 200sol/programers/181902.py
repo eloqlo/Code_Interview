357ca8bd3ae5f5b3f689959032641d7bb12328f4
rrr-jh <dlwoguddd@gmail.com>
2024-04-28 23:59:56 +0900

    daily

--- 200sol/programers/181902.py
+++ 200sol/programers/181902.py
@@ -1,10 +1,9 @@
 def solution(my_string):
-    answer = [0 for _ in range(52)]
-    
-    for s in my_string:
-        if ord(s)<97:
-            answer[ord(s)-65] += 1
-        else:
-            answer[ord(s)-97+26] += 1
-    
+    anchor = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    answer = [0] * len(anchor)
+    for ele in my_string:
+        for idx in range(len(anchor)):
+            if ele==anchor[idx]:
+                break
+        answer[idx]+=1
     return answer
