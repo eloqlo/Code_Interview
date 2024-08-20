6254b721324830bc9da6159df6c0cf82563b96bd
rrr-jh <dlwoguddd@gmail.com>
2024-08-20 21:59:25 +0900

    weird problem

--- 200sol/baekjoon/1911.py
+++ 200sol/baekjoon/1911.py
@@ -1,15 +1,29 @@
-# 존나어렵, 흙길 채우기
-n, l = map(int, input().split())
-arr1=[]
-for _ in range(n):
-    str,end = map(int,input().split())
-    arr1.append([str,end])  # 인덱스로 저장.
+N, L = map(int, input().split())
+pd=[]
+for _ in range(N):
+    st,ed = map(int,input().split())
+    pd.append([st,ed-1])  # 인덱스로 저장.
+pd.sort()
+pd.append((-1,-1))  # end flag
 
-arr1.sort()
-maximum = arr1[-1][1]
 count = 0
+st = pd[0][0]
+for idx in range(N):
+    pst, ped = pd[idx]
+    pl = ped - pst + 1  # 웅덩이 길이
+    X = pl//L + int(bool(pl%L)) # 필요 널빤지 개수
 
-# 너무 복잡하게 짠다. 다시 생각해보기.
+    if st <= pst:  # 이전 널빤지가 영향 X --> 새로 X개 놓기
+        count += X
+        st = pst + X*L
+    elif st > pst:
+        if ped <= st-1:     # 이전 널빤지가 지금꺼 다 덮음
+            continue
+        elif ped > st-1:    # 이전 널빤지가 지금꺼 부분 덮음
+            pl = ped - st + 1
+            X = pl//L + int(bool(pl%L))  # 필요 널빤지 개수
 
+            count += X
+            st += X*L
 
 print(count)
--- 200sol/baekjoon/2565.py
+++ 200sol/baekjoon/2565.py
@@ -1,62 +1,71 @@
-# ! Wrong !
 
-# # input - 순서쌍
+
+
+
+
+
+
+# # ! Wrong !
+#
+# # # input - 순서쌍
+# # n = int(input())
+# # li=[]
+# # for _ in range(n):
+# #     a_sample, b_sample = map(int, input().split())
+# #     li.append((a_sample,b_sample))
+#
+# # # index별 꼬인개수 파악 function
+# # def how_many(li):
+# #     a_i2n = []
+# #     # O(N^2 < 10000)
+# #     for a_a,a_b in li:
+# #         count=0
+# #         for b_a,b_b in li:
+# #             if a_a==b_a:
+# #                 continue
+# #             if a_a>b_a and a_b<b_b:
+# #                 count+=1
+# #             if a_a<b_a and a_b>b_b:
+# #                 count+=1
+# #         a_i2n.append(count) # 해당 a랑 겹친 개수를 list에 저장
+# #     return a_i2n
+#
+# # count = 0
+# # a_i2n = how_many(li) # 시작시점 꼬인 list
+# # while(sum(a_i2n)!=0):
+# #     worst_i = a_i2n.index(max(a_i2n))
+# #     # li에서 제일 꼬인거 제거
+# #     li.pop(worst_i)
+# #     a_i2n = how_many(li)
+# #     count+=1    # counting
+#
+# # print(count)
+#
+# ########################################################################
+#
 # n = int(input())
-# li=[]
-# for _ in range(n):
-#     a_sample, b_sample = map(int, input().split())
-#     li.append((a_sample,b_sample))
-
-# # index별 꼬인개수 파악 function
-# def how_many(li):
-#     a_i2n = []
-#     # O(N^2 < 10000)
-#     for a_a,a_b in li:
-#         count=0
-#         for b_a,b_b in li:
-#             if a_a==b_a:
-#                 continue
-#             if a_a>b_a and a_b<b_b:
-#                 count+=1
-#             if a_a<b_a and a_b>b_b:
-#                 count+=1
-#         a_i2n.append(count) # 해당 a랑 겹친 개수를 list에 저장
-#     return a_i2n
-
-# count = 0
-# a_i2n = how_many(li) # 시작시점 꼬인 list
-# while(sum(a_i2n)!=0):
-#     worst_i = a_i2n.index(max(a_i2n))
-#     # li에서 제일 꼬인거 제거
-#     li.pop(worst_i)
-#     a_i2n = how_many(li)
-#     count+=1    # counting
-    
-# print(count)
-
-########################################################################
-
-n = int(input())
-a = sorted([list(map(int, input().split())) for _ in range(n)])
-
-# 연속 증가순열 개수
-
-dp=[]
-for i in range(n):
-    count=0
-    for j in range(i+1,n):
-        if a[i][1]<a[j][1]:
-            count+=1
-    dp.append(count)
-
-a.sort(reverse=True)
-for i in range(n):
-    count=0
-    for j in range(i+1,n):
-        if a[i][1]<a[j][1]:
-            count+=1
-    dp.append(count)
-
-
-
-print(n-max(dp))
+# a = sorted([list(map(int, input().split())) for _ in range(n)])
+#
+# # 연속 증가순열 개수
+#
+# dp=[]
+# for i in range(n):
+#     count=0
+#     for j in range(i+1,n):
+#         if a[i][1]<a[j][1]:
+#             count+=1
+#     dp.append(count)
+#
+# a.sort(reverse=True)
+# for i in range(n):
+#     count=0
+#     for j in range(i+1,n):
+#         if a[i][1]<a[j][1]:
+#             count+=1
+#     dp.append(count)
+#
+#
+#
+# print(n-max(dp))
+#
+# ########################################################################
