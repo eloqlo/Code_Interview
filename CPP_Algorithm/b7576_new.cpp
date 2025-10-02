0c0198dceb862e05deab2d79802b4f4790c53733
rrr-jh <dlwoguddd@gmail.com>
2025-10-02 09:42:02 +0900

    bfs

--- CPP_Algorithm/b7576_new.cpp
+++ CPP_Algorithm/b7576_new.cpp
@@ -1,70 +1,89 @@
-#include <deque>
 #include <iostream>
+#include <vector>
+#include <deque>
 using namespace std;
 
-int arr[1001][1001];
-bool visited[1001][1001];
-int dx[4] = {-1,1,0,0};
-int dy[4] = {0,0,1,-1};
-int n, m, ans = -1;
 typedef struct{
-    int x;
-    int y;
+    int r;
+    int c;
+} loc;
+typedef struct{
+    int val;
+    bool visit;
 } tmt;
-deque<tmt> v;
 
-void bfs(tmt cur)
-{
-    visited[cur.x][cur.y] = 1;
-    for (int i=0; i<4; i++)
-    {
-        int nx = cur.x + dx[i];
-        int ny = cur.y + dy[i];
-        if (nx<0 || ny<0 || nx>=m || ny>=n)
-            continue;
-        if (visited[nx][ny] || arr[nx][ny]!=0)
-            continue;
-        visited[nx][ny] = 1;
-        arr[nx][ny] = arr[cur.x][cur.y] + 1;
-        v.push_back({nx,ny});
-    }
-}
+int dr[4] = {1,-1,0,0};
+int dc[4] = {0,0,1,-1};
+int M, N;
+vector<vector<tmt>> arr;
+deque<loc> dq;
+int space_cnt = 0;
 
-int main()
-{
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    for (int i = 0; i < m; i++)
+    // INITIALIZATION
+    /* arr, dq */
+    cin >> M >> N;
+    int val;
+    for (int r=0; r<N; r++)
     {
-        for (int j=0; j<n; j++)
+        vector<tmt> line;
+        for (int c=0; c<M; c++)
         {
-            cin >> arr[i][j];
-            if (arr[i][j] == 1)
-                v.push_back({i,j});
-        }
-    }
-
-    while (!v.empty())
-    {
-        tmt start = v.front();
-        v.pop_front();
-        bfs(start);
-    }
-
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[i][j] == 0)
+            cin >> val;
+            line.push_back({val,false});
+            if (val == 1)
             {
-                cout << -1;
-                return 0;
+                dq.push_back({r,c});
+                line[c].visit = true;
             }
-            else
-                ans = max(ans, arr[i][j]);
+            else if (val == 0)
+                space_cnt++;
         }
+        arr.push_back(line);
     }
-    cout << ans - 1;
+
+    if (space_cnt == 0)
+    {
+        cout << 0;
+        return 0;
+    }
+
+    // ALGORITHM
+    int day = 0;
+    while(dq.size() > 0)
+    {
+        deque<loc> nxt_dq;
+        for(loc tmt_loc : dq)
+        {
+            for (int di=0; di<4; di++)
+            {
+                int nr = tmt_loc.r + dr[di];
+                int nc = tmt_loc.c + dc[di];
+                if (nr<0 || nr>=N || nc<0 || nc>=M)
+                    continue;
+                if (arr[nr][nc].val != 0 || arr[nr][nc].visit)
+                    continue;
+                
+                arr[nr][nc].visit = true;
+                arr[nr][nc].val = 1;
+                nxt_dq.push_back({nr,nc});
+                space_cnt--;
+            }
+        }
+        if (nxt_dq.size() > 0)
+        {
+            day++;
+        }
+        dq.swap(nxt_dq);
+    }
+
+    if (space_cnt==0)
+        cout << day;
+    else   
+        cout << -1;
+    return 0;
 }
