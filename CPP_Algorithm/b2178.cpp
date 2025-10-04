2572255fc58472bf8de67ffdbc45445da43b2e2c
rrr-jh <dlwoguddd@gmail.com>
2025-10-04 11:31:38 +0900

    bfs solved

--- CPP_Algorithm/b2178.cpp
+++ CPP_Algorithm/b2178.cpp
@@ -18,68 +18,63 @@ vector<vector<arr_val>> arr;
 deque<loc> dq;
 deque<loc> nxt_dq;
 int N,M;
-loc target_loc = {N-1,M-1};
 int dr[4] = {-1,1,0,0};
 int dc[4] = {0,0,-1,1};
 bool is_finished = false;
 
-void arr_bfs(deque<loc> dq);
+void arr_bfs(deque<loc> &dq);
 int char_to_int(char num);
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> N >> M;
+    vector<arr_val> line_vec;
     for (int r=0; r<N; r++){
         string line;
         cin >> line;
         for (char val : line){
-            arr[r].push_back({char_to_int(val),false});
+            line_vec.push_back({char_to_int(val),false});
         }
+        arr.push_back(line_vec);
+        line_vec.clear();
     }
-    arr[0][0].visit = true;
     
-    int move = 0;
-    while(dq.size() > 0){
+    arr[0][0].visit = true;
+    dq.push_back({0,0});
+
+    int move = 1;
+    while(!dq.empty()){
         arr_bfs(dq);
         dq.swap(nxt_dq);
         nxt_dq.clear();
 
-        if (dq.size()==0){
-            break;
-        }
         move++;
-        if (is_finished){
-            break;
-        }
+        if (is_finished) break;
+        if (dq.empty()) break;
     }
 
-    cout << move;
+    cout << move << endl;
     return 0;
 }
 
 
 
 /* 현재 level에서 조회할 위치 dq를 받아서 arr를 업데이트해준다. */
-void arr_bfs(deque<loc> dq){
+void arr_bfs(deque<loc> &dq){
     for (loc cur : dq){
         for (int di=0; di<4; di++){
             int nr = cur.r + dr[di];
             int nc = cur.c + dc[di];
-            if (nr<0 || nr > N-1 || nc<0 || nc > M-1){
-                continue;
-            }
-            if (arr[nr][nc].val == 0 || arr[nr][nc].visit == true){
-                continue;
-            }
-            if (nr==N-1 && nc==M-1){
-                is_finished = true;
-            }
+            if (nr<0 || nr >= N || nc<0 || nc >= M) continue;
+            if (arr[nr][nc].val == 0 || arr[nr][nc].visit == true) continue;
             arr[nr][nc].visit = true;
+            if (nr==N-1 && nc==M-1) is_finished = true;
             nxt_dq.push_back({nr,nc});
         }
+        if (is_finished) break;
     }
 }
 
